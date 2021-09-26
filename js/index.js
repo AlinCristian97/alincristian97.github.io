e79539d1af48165f71664e39f78ab3ba670c4476
Alin Apostol <alin.cristian977@gmail.com>
2021-09-26 11:57:46 +0300

    Changed avatar image & added project logos

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
-﻿// const navToggle = document.querySelector('.nav-toggle');
-// const navLinks = document.querySelectorAll('.nav__link');
-//
-// navToggle.addEventListener('click', () => {
-//     document.body.classList.toggle('nav-open');
-// });
-//
-// navLinks.forEach(link => {
-//    link.addEventListener('click', () => {
-//        document.body.classList.remove('nav-open');
-//    });
-// });
+﻿function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
