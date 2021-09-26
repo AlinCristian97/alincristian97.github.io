2a28e267de31f63b5fe35aec89a66c9ccc248844
Alin Apostol <alin.cristian977@gmail.com>
2021-09-26 16:46:56 +0300

    Added 'Toggle' div in 'My Projects' section

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-﻿function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+﻿function showOnly2DProjects() {
+    const projects2dDiv = document.getElementById("projects-2d");
+    const projects3dDiv = document.getElementById("projects-3d");
+
+    projects2dDiv.style.display = "block"
+    projects3dDiv.style.display = "none";
+}
+
+function showOnly3DProjects() {
+    const projects2dDiv = document.getElementById("projects-2d");
+    const projects3dDiv = document.getElementById("projects-3d");
+
+    projects3dDiv.style.display = "block"
+    projects2dDiv.style.display = "none";
 }
