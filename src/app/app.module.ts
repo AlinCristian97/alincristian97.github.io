819eebcf3d77744729334443a2fc2015c55dfe26
Alin Apostol <alin.cristian977@gmail.com>
2022-03-06 03:41:47 +0200

    generated files for technologies and languages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/home/home.component';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { LanguagesComponent } from './languages/languages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    TechnologiesComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule
